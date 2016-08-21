7423da4e07512242db785e77bc39e20d9b237847
David Wright <dave@ritey.com>
2016-08-21 19:53:11 +0100

    Text update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Laravel</title>
+        <title>ritey.com</title>
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Ritey.com</div>
+                <div class="title">ritey.com</div>
             </div>
         </div>
     </body>
