ad46886dfd0f0a498d1966abca358414a3cfb2ad
David Wright <dave@ritey.com>
2016-08-30 10:25:02 +0100

    Add another image

--- coderstudios/Commands/Tweets.php
+++ coderstudios/Commands/Tweets.php
@@ -32,7 +32,11 @@ class Tweets {
 				];
 				$this->tweets->update($tweet->id, $data);
 				if ($tweet->name == 'Free Photos') {
-					$result = $this->twitter->query('media/upload', 'POST', ['media' => file_get_contents(public_path() . '/images/kitten-2-21-08-2016.jpg')],true);
+
+					$images[] = file_get_contents(public_path() . '/images/kitten-2-21-08-2016.jpg';
+					$images[] = file_get_contents(public_path() . '/images/beach-18-08-2016.jpg';
+
+					$result = $this->twitter->query('media/upload', 'POST', ['media' => $images[mt_rand(0,count($images)-1)])],true);
 					$result = $this->twitter->post('statuses/update', ['media_ids' => $result->media_id, 'status' => $tweet->tweet, 'format' => 'json']);
 				} else {
 					$result = $this->twitter->post('statuses/update', ['status' => $tweet->tweet, 'format' => 'json']);
